d932ae7ac1a43a9961941e4982ccc79d2f1fc00d
AKT <>
2020-10-30 11:38:38 +0530

    react test zeevaa.github.io

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "./logo.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <img src={logo} className="App-logo" alt="logo" />
+    </div>
+  );
+}
+
+export default App;
